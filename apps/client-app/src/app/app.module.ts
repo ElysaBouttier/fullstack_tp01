cc37438037f0466d56cde0080deacf8983850bfd
ElysaBouttier <e.bouttier@hotmail.fr>
2021-11-04 14:26:38 +0100

    add routing

--- apps/client-app/src/app/app.module.ts
+++ apps/client-app/src/app/app.module.ts
@@ -7,11 +7,26 @@ import { AnimalsComponent } from './animals/animals.component';
 import { AnimalsListComponent } from './animals/animals-list/animals-list.component';
 import { AnimalsDetailsComponent } from './animals/animals-details/animals-details.component';
 
+const routes = [
+  {
+    path: 'animals',
+    component: AnimalsDetailsComponent,
+  },
+  {
+    path: 'animals-list',
+    component: AnimalsListComponent,
+  },
+];
 @NgModule({
-  declarations: [AppComponent, AnimalsComponent, AnimalsListComponent, AnimalsDetailsComponent],
+  declarations: [
+    AppComponent,
+    AnimalsComponent,
+    AnimalsListComponent,
+    AnimalsDetailsComponent,
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([routes], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
