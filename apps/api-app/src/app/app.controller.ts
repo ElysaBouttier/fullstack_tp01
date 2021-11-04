547dcde9310fa2eeac653c1ff338275f80cd61ba
ElysaBouttier <e.bouttier@hotmail.fr>
2021-11-04 17:15:27 +0100

    stop project

--- apps/api-app/src/app/app.controller.ts
+++ apps/api-app/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -6,8 +6,34 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getData() {
+  //   return this.appService.getData();
+  // }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() pet: any){
+    return this.appService.create(pet);
+  }
+
   @Get()
-  getData() {
-    return this.appService.getData();
+  findAll() {
+    return this.appService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.appService.findOne(+id);
+  }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() pet: Pet) {
+  //   return this.appService.update(+id, pet);
+  // }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.appService.remove(+id);
   }
 }
--- apps/api-app/src/app/app.service.ts
+++ apps/api-app/src/app/app.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { AxiosResponse } from 'axios';
+import {map} from 'rxjs/operators';
+
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to api-app!' };
+  constructor(private httpService: HttpService){}
+  pets = [
+    {
+      id: 1,
+      name: 'nom',
+      age: 5,
+      gender: "Male",
+    },
+  ];
+
+  create(pets) {
+    this.pets.push(pets);
+    return 'This action adds a new pet';
+  }
+
+  findAll(): Observable<AxiosResponse<any>> {
+    const response = this.httpService.get('https://meetapet-api.herokuapp.com/api/pets')
+    .pipe(
+      map(response => response.data)
+    );    
+    return response;
+  }
+
+  findOne(id: number) {
+    return this.pets.find((pet) => pet.id === id);
+  }
+
+  update(id: number) {
+    return `This action updates a #${id} pet`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pet`;
   }
 }
